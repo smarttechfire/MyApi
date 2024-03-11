f2c4e9e09369b97550fd413b9e71492219dbd959
smarttechfire <educationfire552@gmail.com>
2024-03-11 18:48:25 +0530

    update

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ mongoose.connect(process.env.MONGO_URI);
 
 
 mongoose.connection.on("connected", () => {
-  console.log("Mongoose connected");
-  app.listen(process.env.PORT, () => console.log("Serc started 3000"));
+  
+  app.listen(process.env.PORT);
 });
 
 mongoose.connection.on("error", (error) => {
