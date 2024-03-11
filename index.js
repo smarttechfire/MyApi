5fff5089e2ff9485bde212bce4450c98657f9109
smarttechfire <educationfire552@gmail.com>
2024-03-11 18:42:30 +0530

    Update

--- index.js
+++ index.js
@@ -8,13 +8,15 @@ app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI);
 
+
+
 mongoose.connection.on("connected", () => {
   console.log("Mongoose connected");
   app.listen(process.env.PORT, () => console.log("Serc started 3000"));
 });
 
 mongoose.connection.on("error", (error) => {
-  console.log("Mongoose connect error", error);
+  console.log("Mongoose connection error", error);
 });
 
 app.use("/products",require("./routes/productsRoute"));
